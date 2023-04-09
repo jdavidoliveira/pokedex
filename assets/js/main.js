211f8c50914181093561eff821eb2c4de6373e18
lordaval <lordaval1117@gmail.com>
2023-04-09 06:32:02 +0000

    projeto finalizado + adição do botão de carregar mais + limite de geração

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,45 @@
-//funçao para transformar a primeira letra em maiuscula
-function primeiraLetraMaiuscula(texto) {
-    let primeiraLetra = texto.charAt(0).toUpperCase();
-    let restoDoTexto = texto.slice(1).toLowerCase();
-    return primeiraLetra + restoDoTexto;
-}
+const pokemonList = document.querySelector('#pokemonList');
+const loadMoreButton = document.querySelector('#loadMoreButton')
 
+const limit = 10;
+let offset = 0;
+maxRecord = 151;
 
 //função pra converter o dado json em uma <li> HTML
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon ${pokemon.type}">
         <span class="number">#${pokemon.number}</span>
-        <span class="name">${primeiraLetraMaiuscula(pokemon.name)}</span>
+        <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${type}">${primeiraLetraMaiuscula(type)}</li>`).join('')}
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
             </ol>
             <img src="${pokemon.photo}" alt="${pokemon.name}">
         </div>
     </li>`;
 }
 
+function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        newHtml = pokemons.map(convertPokemonToLi).join('');
+        pokemonList.innerHTML += newHtml;
+    })
+}
 
+loadPokemonItems(offset, limit)
 
-const pokemonList = document.querySelector('#pokemonList');
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    newHtml = pokemons.map(convertPokemonToLi).join('');
-    pokemonList.innerHTML = newHtml;
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+        alert("Limite alcançado!")
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
 })
